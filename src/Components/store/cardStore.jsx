bb6a7fc43d7c449c243eaea1890338bc8fb8e5f9
azerkazimov <648azer@gmail.com>
2024-04-20 00:50:17 +0400

    cardStore

--- src/Components/store/cardStore.jsx
+++ src/Components/store/cardStore.jsx
@@ -2,7 +2,21 @@ import { create } from "zustand";
 
 const useCardStore = create((set) => ({
   card: [],
-  addToCard: (product) => set((state) => ({ card: [...state.card, product] })),
+  addToCard: (product) =>
+    set((state) => {
+      const existingProductIndex = state.card.findIndex(
+        (prod) => prod.id === product.id
+      );
+
+      switch (existingProductIndex) {
+        case -1:
+          return { card: [...state.card, product] };
+        default:
+          const updatedCard = [...state.card];
+          updatedCard[existingProductIndex].quantity += product.quantity || 1;
+          return { card: updatedCard };
+      }
+    }),
   removeFromCard: (productId) =>
     set((state) => ({
       card: state.card.filter((prod) => prod.id !== productId),
